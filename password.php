2795b799b98f2dc5a01f6f3ce61dabb66fb04b90
sassabe <donadio.isabella@gmail.com>
2016-09-21 15:30:38 -0400

    working password generator, validation not working yet.

--- password.php
+++ password.php
@@ -26,49 +26,65 @@ $symbolsArray = [
 #variables
 	$error ="";
 	$password = "";
-	$add_number ="no";
-	$add_symbol ="no";
-	$count = $_POST['number_of_words'];
-	$max_words = count($wordsArray) -1;
+	//$intValue ="";
+	//$count = $_POST['number_of_words'];
+	//$max_words = count($wordsArray) -1;
 	$number_of_words = 5;
-	
-	
+# validation for number of words
+if (isset($_POST['number_of_words'])) {
+	$number_of_words = $_POST['number_of_words'];
+/* 
+	if(is_int($intValue) && (1 <= $intValue) OR ($intValue <= 5)) {
+	 //valid
+		$number_of_words = $intValue;
+	} else{
+		$error = "Please enter a number between 2 and 5";
+		return;
+	//invalid
+	}*/
+} else {
+	$number_of_words = 5;
+}	
 
-#If user doesn't select a number, generate error message 
-#Error message not showing up, need to fix
+#Generate password: 
 
-if (isset($_POST['submit'])){
-	if (!isset($_POST['number_of_words'])) {
-		$error = "Please select a number :)";
-	} 
+#Create place-saver array to collect all array values
+$randomWords = array_rand($wordsArray, $number_of_words);
+
+# Create password by looping through arrays using place saver array
+foreach ($randomWords as $key => $value) {
+	$password .= $wordsArray[$value]. "-";
 }
-	
-#get data from form
-$formData = $_POST;
-if (!empty($formData['number_of_words'])) {
-	$number_of_words = intval($formData['number_of_words']);
-}
 
-#function - only working for 1 word...
-	for ($i = 0; $i <$number_of_words; $i++) {
-		if ($i == 0) {
-			if ($number_of_words == 2) {
-				$password = ($wordsArray [rand (1, $max_words)]);}
+# remove ending hyphen
+$password = substr($password, 0, -1);
 
-			}
-
-		}
-# not working...
-		
-	if ($add_number == "yes") {
+# add number if checked
+if (isset($_POST ["add_number"])) {
 		$password = $password.rand(0,9);
 	} 
-	 if ($add_symbol == "yes") {
-	 	$password = $password.rand($symbolsArray);
+if (isset($_POST ["add_symbol"])) {
+	 	$password .= $symbolsArray[rand(0,5)];
 	 }
 
+#Check if value is between 2 and 5
 
 
+# Validate input  not working
 
+#if user does not enter a numeric value, warn with error message.
+/*foreach($_POST as $key => $value) {
 
+if (!ctype_digit($value)) {
+	$error = "Please enter a number between 2 and 5";
+	return;
+}
+#if user does not enter anything, warn with error message.
+	if (!empty($value)) {
+	$error = "Please enter a number between 2 and 5";
+	return;
+}
+
+}*/
+?>
 
