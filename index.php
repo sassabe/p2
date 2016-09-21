54f2bf8e3ab0de3b5338dac61ab294400435e05b
sassabe <donadio.isabella@gmail.com>
2016-09-21 15:29:50 -0400

    changes to error message

--- index.php
+++ index.php
@@ -14,16 +14,12 @@
 	<h1>xkcd Password Generator</h1>
 	<p class="password"><?php echo $password; ?></p>
 	<div class="form_options"><form action="index.php" method="POST">
-		<label for="number_of_words">Number of Words</label>
-		<select name="number_of_words" value="<?php echo $_POST["number_of_words"];?>">
-		<span class="error">*<?php echo $error;?></span>
-			<option value="none">Select #</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-		</select><br>
+		<label for="number_of_words">Number of Words (2-5)</label>
+		<input type="text" maxlength="1" name="number_of_words">
+		<?php if(isset($error)): ?>
+		<div class="error"><?php echo $error;?></div>
+		<?php endif?> <!-- from class notes -->
+		<br>
 		<input type="checkbox" name="add_number" value="number"></input><label for="add_number">Add a number</label><br>
 		<input type="checkbox" name="add_symbol" value="symbol"></input><label for="add_symbol">Add a symbol</label><br>
 
@@ -34,7 +30,5 @@
 		<p><a href="http://xkcd.com/936/">xkcd password strength</a></p>
 		<img src="images/unicorn-rhino.gif">
 	</div>
-	<!--<h1>The time is <?php echo $now; ?></h1>
-	<img src="http://making-the-internet.s3.amazonaws.com/php-<?php echo $timeOfDay?>.png">-->
 </body>
 </htmml>
